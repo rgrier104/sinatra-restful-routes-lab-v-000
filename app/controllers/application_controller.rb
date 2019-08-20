b75568fade44111d2a30cfabe434f4d61c853203
Rachel Grier <rgrier104@gmail.com>
2019-08-20 20:37:57 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -41,7 +41,7 @@ class ApplicationController < Sinatra::Base
     end
 
     post '/recipes' do
-      recipe = recipe.create(title: params[:title], content: params[:content])
+      recipe = Recipe.create(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
 
       redirect to "/recipes/#{recipe.id}"
     end
