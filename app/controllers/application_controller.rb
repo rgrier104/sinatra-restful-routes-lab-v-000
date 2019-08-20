567210c05b28e21fd6c25f52b14a83a9c3ef1236
Rachel Grier <rgrier104@gmail.com>
2019-08-20 20:37:00 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,7 +21,7 @@ class ApplicationController < Sinatra::Base
     end
 
     get '/recipes/:id/edit' do
-      @recipe = recipe.find(params[:id])
+      @recipe = Recipe.find(params[:id])
 
       erb :edit
     end
