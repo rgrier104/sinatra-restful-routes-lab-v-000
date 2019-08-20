3a9d1c0d250a0818573658aeb24a16867592e5ad
Rachel Grier <rgrier104@gmail.com>
2019-08-20 20:35:48 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,46 +4,46 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/recipes' do
-    @recipes = Recipe.all
+  get '/articles' do
+      @articles = Article.all
 
-    erb :index
-  end
+      erb :index
+    end
 
-  get '/recipes/new' do
-    erb :new
-  end
+    get '/articles/new' do
+      erb :new
+    end
 
-  get '/recipes/:id' do
-    @recipe = Recipe.find(params[:id])
+    get '/articles/:id' do
+      @article = Article.find(params[:id])
 
-    erb :show
-  end
+      erb :show
+    end
 
-  get '/recipes/:id/edit' do
-    @recipe = Recipe.find(params[:id])
+    get '/articles/:id/edit' do
+      @article = Article.find(params[:id])
 
-    erb :edit
-  end
+      erb :edit
+    end
 
-  patch '/recipes/:id' do
-    recipe = Recipe.find(params[:id])
-    recipe.update(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
+    patch '/articles/:id' do
+      article = Article.find(params[:id])
+      article.update(title: params[:new_title], content: params[:new_content])
 
-    redirect to "/recipes/#{recipe.id}"
-  end
+      redirect to "/articles/#{article.id}"
+    end
 
-  delete '/recipes/:id/delete' do
-    recipe = Recipe.find(params[:id])
-    recipe.destroy
+    delete '/articles/:id/delete' do
+      article = Article.find(params[:id])
+      article.destroy
 
-    redirect to '/recipes'
-  end
+      redirect to '/articles'
+    end
 
-  post '/recipes' do
-    recipe = Recipe.create(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
+    post '/articles' do
+      article = Article.create(title: params[:title], content: params[:content])
 
-    redirect to '/recipes/#{recipe.id}'
-  end
+      redirect to "/articles/#{article.id}"
+    end
 
 end
